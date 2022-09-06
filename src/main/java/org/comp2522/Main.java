0a703fea8482770153d6005a713662626ecba760
spicious225 <ryanthelion.c@gmail.com>
2022-09-06 11:23:22 -0700

    fixed check style errors

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -1,7 +1,11 @@
 package org.comp2522;
 
+/**
+ * this function prints to terminal.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("my favourite animal is dog");
-    }
+  public static void main(String[] args) {
+
+    System.out.println("my favourite animal is dog");
+  }
 }
